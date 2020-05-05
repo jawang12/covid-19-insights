f19852f738095fa82b9625cde8436ac61df6faae
Jimmy Wang <jawang12@gmail.com>
2020-05-05 19:30:08 -0400

    fetch data from covid19 api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 
+import { Cards, CountryPicker, Chart } from './components';
+import classes from './App.module.css';
+import { fetchData } from './api';
+
 export default class App extends Component {
+  state = {
+    data: {}
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+    this.setState({ data });
+  }
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className={classes.Container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
   }
 }
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountryPicker = (props) => {
+  return <div>CountryPicker</div>;
+};
+
+export default CountryPicker;
